dc3fd885ac97203639667079cd0361e758963bfe
Jiri Pospisil <mekishizufu@gmail.com>
2015-05-13 22:45:18 +0200

    Initial commit

---
+++ src/com/jpospisil/GoToTabAction.java
@@ -0,0 +1,44 @@
+package com.jpospisil;
+
+import com.intellij.openapi.actionSystem.*;
+import com.intellij.openapi.fileEditor.ex.FileEditorManagerEx;
+import com.intellij.openapi.fileEditor.impl.EditorWindow;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.wm.ToolWindowManager;
+
+public class GoToTabAction extends AnAction {
+    private final int index;
+
+    public GoToTabAction(String text, final int index) {
+        super(text);
+        this.index = index;
+    }
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        DataContext dataContext = e.getDataContext();
+        Project project = CommonDataKeys.PROJECT.getData(dataContext);
+        if (project == null) {
+            return;
+        }
+
+        ToolWindowManager windowManager = ToolWindowManager.getInstance(project);
+
+        if (windowManager.isEditorComponentActive()) {
+            goToTab(dataContext, project);
+        }
+    }
+
+    private void goToTab(DataContext dataContext, Project project) {
+        final FileEditorManagerEx editorManager = FileEditorManagerEx.getInstanceEx(project);
+        EditorWindow currentWindow = EditorWindow.DATA_KEY.getData(dataContext);
+        if (currentWindow == null) {
+            currentWindow = editorManager.getCurrentWindow();
+        }
+        final VirtualFile[] files = currentWindow.getFiles();
+        if (index < files.length) {
+            editorManager.openFile(files[index], true);
+        }
+    }
+}
---
+++ src/com/jpospisil/RegisterGoToTabsComponent.java
@@ -0,0 +1,45 @@
+package com.jpospisil;
+
+import com.intellij.openapi.actionSystem.ActionManager;
+import com.intellij.openapi.actionSystem.DefaultActionGroup;
+import com.intellij.openapi.components.ApplicationComponent;
+import org.jetbrains.annotations.NotNull;
+
+public class RegisterGoToTabsComponent implements ApplicationComponent {
+    @Override
+    public void initComponent() {
+        final ActionManager am = ActionManager.getInstance();
+        final DefaultActionGroup windowM = (DefaultActionGroup) am.getAction("EditorTabsGroup");
+
+        final DefaultActionGroup goToTabsGroup = new DefaultActionGroup("Go To Tabs", true);
+        addActions(goToTabsGroup, am);
+
+        windowM.add(goToTabsGroup);
+    }
+
+    private void addActions(final DefaultActionGroup goToTabsGroup, final ActionManager am) {
+        for (int i = 0; i < 10; i++) {
+            addAction(i, goToTabsGroup, am);
+        }
+    }
+
+    private void addAction(final int index, final DefaultActionGroup goToTabGroup,
+                           final ActionManager am) {
+        final String actionName = "Go To Tab #" + index;
+
+        final GoToTabAction action = new GoToTabAction(actionName, index);
+
+        goToTabGroup.add(action);
+        am.registerAction(actionName, action);
+    }
+
+    @Override
+    public void disposeComponent() {
+    }
+
+    @NotNull
+    @Override
+    public String getComponentName() {
+        return "RegisterGoToTabsComponent";
+    }
+}
